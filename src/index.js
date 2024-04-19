22f6147d2e1935369193132cd67c666457b2a6a4
nfrank5 <17028123+nfrank5@users.noreply.github.com>
2024-04-19 13:37:21 +0800

    Adding configuration to webpack

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
+import './style.css';
+
+//Como importar imagenes
+//import Icon from './icon.png';
+//const myIcon = new Image();
+//myIcon.src = Icon;
+//element.appendChild(myIcon);
+
 function component() {
   const element = document.createElement('div');
 
